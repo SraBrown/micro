044c6ed0fc3e8f7c46cdbd41cd2ab436079afce6
Brown <73099195+SraBrown@users.noreply.github.com>
2023-08-04 22:25:08 +0200

    Create Microservice

---
+++ opentrends-service/src/main/java/com/xm/opentrendsservice/OpentrendsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xm.opentrendsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentrendsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentrendsServiceApplication.class, args);
+	}
+
+}
---
+++ opentrends-service/src/test/java/com/xm/opentrendsservice/OpentrendsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xm.opentrendsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentrendsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
