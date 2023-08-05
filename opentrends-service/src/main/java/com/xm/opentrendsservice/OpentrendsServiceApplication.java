6b41affdb8499f6e3ad1c8c95153f3b02fcda1c4
SraBrown <ximenamoreno.freelance@gmail.com>
2023-08-05 22:57:53 +0200

    add git ignore

--- opentrends-service/src/main/java/com/xm/opentrendsservice/OpentrendsServiceApplication.java
+++ opentrends-service/src/main/java/com/xm/opentrendsservice/OpentrendsServiceApplication.java
@@ -10,4 +10,5 @@ public class OpentrendsServiceApplication {
 		SpringApplication.run(OpentrendsServiceApplication.class, args);
 	}
 
+	
 }
--- opentrends-service/src/test/java/com/xm/opentrendsservice/OpentrendsServiceApplicationTests.java
+++ opentrends-service/src/test/java/com/xm/opentrendsservice/OpentrendsServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.xm.opentrendsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 class OpentrendsServiceApplicationTests {
