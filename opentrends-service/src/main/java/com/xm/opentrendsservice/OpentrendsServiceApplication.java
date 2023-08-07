d40d4f9f5b29c0c38df12c5937b1de2fee3dcb0d
SraBrown <ximenamoreno.freelance@gmail.com>
2023-08-07 12:52:00 +0200

    Config swaggerOpenAPI

--- opentrends-service/src/main/java/com/xm/opentrendsservice/OpentrendsServiceApplication.java
+++ opentrends-service/src/main/java/com/xm/opentrendsservice/OpentrendsServiceApplication.java
@@ -2,6 +2,12 @@ package com.xm.opentrendsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 
 @SpringBootApplication
 public class OpentrendsServiceApplication {
@@ -10,5 +16,15 @@ public class OpentrendsServiceApplication {
 		SpringApplication.run(OpentrendsServiceApplication.class, args);
 	}
 
-	
+	 @Bean
+	    public OpenAPI customOpenAPI() {
+	        return new OpenAPI()
+	                .info(new Info()
+	                        .title("opentrends API -------")
+	                        .version("0.11")
+	                        .description("opentrends")
+	                        .termsOfService("https://www.opentrends.net/es")
+	                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+	    }
 }
