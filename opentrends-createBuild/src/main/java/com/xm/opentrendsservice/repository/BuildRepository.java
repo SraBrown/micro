9a3a83a34ca5208b7acc3424981141a75d09ec9c
SraBrowm <ximenamoreno.freelance@gmail.com>
2023-08-09 09:14:38 +0200

    add repository

--- opentrends-createBuild/src/main/java/com/xm/opentrendsservice/repository/BuildRepository.java
+++ opentrends-createBuild/src/main/java/com/xm/opentrendsservice/repository/BuildRepository.java
@@ -3,6 +3,7 @@ package com.xm.opentrendsservice.repository;
 import org.springframework.data.repository.CrudRepository;
 import com.xm.opentrendsservice.models.Builds;
 
+
 public interface BuildRepository extends CrudRepository<Builds, Long>{
 
 }
